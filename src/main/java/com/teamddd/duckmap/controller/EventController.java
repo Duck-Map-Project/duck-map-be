bf9d54f2b8b3f0a349c613e5e4f918c55b2affa5
yurim <yrim113@gmail.com>
2023-05-13 17:19:10 +0900

    Feat: #4 - 이벤트 pk로 조회하는 api 생성
    EventRes에 ArtistRes, CategoryRes는 mock으로 대체

--- src/main/java/com/teamddd/duckmap/controller/EventController.java
+++ src/main/java/com/teamddd/duckmap/controller/EventController.java
@@ -1,6 +1,11 @@
 package com.teamddd.duckmap.controller;
 
+import java.time.LocalDateTime;
+import java.util.List;
+
 import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -8,6 +13,8 @@ import org.springframework.web.bind.annotation.RestController;
 
 import com.teamddd.duckmap.dto.CreateEventReq;
 import com.teamddd.duckmap.dto.CreateEventRes;
+import com.teamddd.duckmap.dto.EventRes;
+import com.teamddd.duckmap.dto.ImageRes;
 import com.teamddd.duckmap.dto.Result;
 
 import io.swagger.v3.oas.annotations.Operation;
@@ -32,4 +39,30 @@ public class EventController {
 			.build();
 	}
 
+	@Operation(summary = "이벤트 pk로 조회")
+	@GetMapping("/{id}")
+	public Result<EventRes> getEvent(@PathVariable Long id) {
+		return Result.<EventRes>builder()
+			.data(
+				EventRes.builder()
+					.id(id)
+					.storeName("상호명")
+					.fromDate(LocalDateTime.now().minusDays(2L))
+					.toDate(LocalDateTime.now().plusDays(1L))
+					.address("주소")
+					.businessHour("10:00 - 18:00")
+					.hashtag("#뫄뫄 #생일축하해")
+					.twitterUrl("https://twitter.com/home?lang=ko")
+					.artists(List.of())
+					.categories(List.of())
+					.images(List.of(
+						ImageRes.builder()
+							.apiUrl("/images/")
+							.filename("filename.jpg")
+							.build()
+					))
+					.build()
+			)
+			.build();
+	}
 }
