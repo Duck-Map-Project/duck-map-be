357508504ddff0203e5e56024405aa071c269231
yurim <yrim113@gmail.com>
2023-05-13 22:29:06 +0900

    Feat: #4 - 이미지 조회 api 생성
    구현하고 img.png 파일 제거 예정

--- src/main/java/com/teamddd/duckmap/controller/ImageController.java
+++ src/main/java/com/teamddd/duckmap/controller/ImageController.java
@@ -1,6 +1,10 @@
 package com.teamddd.duckmap.controller;
 
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.core.io.Resource;
 import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -31,5 +35,11 @@ public class ImageController {
 					.build()
 			)
 			.build();
+	}
+
+	@Operation(summary = "이미지 조회")
+	@GetMapping("/{filename}")
+	public Resource loadImage(@PathVariable String filename) {
+		return new ClassPathResource("static/mock/img.png");
 	}
 }
