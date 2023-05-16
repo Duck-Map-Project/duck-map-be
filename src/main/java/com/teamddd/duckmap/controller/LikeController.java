292cb7efa2e756cccac45746ab9a968dd4fe7484
yk park <busymidnight17@gmail.com>
2023-05-16 23:01:52 +0900

    Fix: #4 - 이벤트 좋아요 api 수정 HttpSession에 저장된 userid 사용할 예정 사용하지 않아도 되는 LikeReq 삭제

--- src/main/java/com/teamddd/duckmap/controller/LikeController.java
+++ src/main/java/com/teamddd/duckmap/controller/LikeController.java
@@ -1,14 +1,13 @@
 package com.teamddd.duckmap.controller;
 
-import org.springframework.validation.annotation.Validated;
+import javax.servlet.http.HttpSession;
+
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.teamddd.duckmap.dto.LikeReq;
 import com.teamddd.duckmap.dto.LikeRes;
 import com.teamddd.duckmap.dto.Result;
 
@@ -19,12 +18,12 @@ import lombok.extern.slf4j.Slf4j;
 @Slf4j
 @RestController
 @RequiredArgsConstructor
-@RequestMapping("/events/{id}/likes")
+@RequestMapping("/events")
 public class LikeController {
 
 	@Operation(summary = "이벤트 좋아요")
-	@PostMapping
-	public Result<LikeRes> likeEvent(@Validated @RequestBody LikeReq likeReq) {
+	@PostMapping("/{id}/likes")
+	public Result<LikeRes> likeEvent(@PathVariable Long id, HttpSession session) {
 		return Result.<LikeRes>builder()
 			.data(
 				LikeRes.builder()
@@ -35,7 +34,7 @@ public class LikeController {
 	}
 
 	@Operation(summary = "이벤트 좋아요 취소")
-	@DeleteMapping
+	@DeleteMapping("/{id}/likes")
 	public Result<Void> deleteLikeEvent(@PathVariable Long id) {
 		return Result.<Void>builder().build();
 	}
