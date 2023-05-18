af398dd37cf83979bcf2955f77651c28d810a153
yurim <yrim113@gmail.com>
2023-05-18 15:43:19 +0900

    Fix: 이벤트 생성 request validation 적용
    상호명, 주소, 이벤트 기간 필수 입력
    아티스트 size 1이상만 가능

--- src/main/java/com/teamddd/duckmap/dto/CreateEventReq.java
+++ src/main/java/com/teamddd/duckmap/dto/CreateEventReq.java
@@ -3,17 +3,27 @@ package com.teamddd.duckmap.dto;
 import java.time.LocalDate;
 import java.util.List;
 
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+
 import lombok.Getter;
 
 @Getter
 public class CreateEventReq {
+	@NotBlank
 	private String storeName;
+	@NotNull
 	private LocalDate fromDate;
+	@NotNull
 	private LocalDate toDate;
+	@NotBlank
 	private String address;
 	private String businessHour;
 	private String hashtag;
 	private String twitterUrl;
+	@NotNull
+	@Size(min = 1)
 	private List<Long> artistIds;
 	private List<Long> categoryIds;
 	private List<String> imageFilenames;
