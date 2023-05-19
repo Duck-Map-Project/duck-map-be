c4476233557181d841747b42e96d71d17c4e0483
yurim <yrim113@gmail.com>
2023-05-19 14:56:08 +0900

    Feat: #4 - 나의 리뷰 목록 조회 api 생성

--- src/main/java/com/teamddd/duckmap/controller/ReviewController.java
+++ src/main/java/com/teamddd/duckmap/controller/ReviewController.java
@@ -3,6 +3,9 @@ package com.teamddd.duckmap.controller;
 import java.time.LocalDateTime;
 import java.util.List;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageImpl;
+import org.springframework.data.domain.Pageable;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -16,6 +19,7 @@ import org.springframework.web.bind.annotation.RestController;
 import com.teamddd.duckmap.dto.CreateReviewReq;
 import com.teamddd.duckmap.dto.CreateReviewRes;
 import com.teamddd.duckmap.dto.ImageRes;
+import com.teamddd.duckmap.dto.MyReviewsRes;
 import com.teamddd.duckmap.dto.Result;
 import com.teamddd.duckmap.dto.ReviewRes;
 import com.teamddd.duckmap.dto.UpdateReviewReq;
@@ -78,5 +82,37 @@ public class ReviewController {
 	@DeleteMapping("/{id}")
 	public Result<Void> deleteReview(@PathVariable Long id) {
 		return Result.<Void>builder().build();
+	}
+
+	@Operation(summary = "나의 리뷰 목록 조회")
+	@GetMapping("/myreview")
+	public Page<MyReviewsRes> getMyReviews(Pageable pageable) {
+		ImageRes imageRes = ImageRes.builder()
+			.apiUrl("/images/")
+			.filename("filename.png")
+			.build();
+
+		return new PageImpl<>(List.of(
+			MyReviewsRes.builder()
+				.id(1L)
+				.eventId(1L)
+				.eventImage(imageRes)
+				.eventStoreName("이벤트 상호명")
+				.createAt(LocalDateTime.now().minusDays(10))
+				.score(4)
+				.reviewImage(imageRes)
+				.content("리뷰 내용")
+				.build(),
+			MyReviewsRes.builder()
+				.id(2L)
+				.eventId(2L)
+				.eventImage(imageRes)
+				.eventStoreName("이벤트 상호명2")
+				.createAt(LocalDateTime.now().minusDays(8))
+				.score(5)
+				.reviewImage(imageRes)
+				.content("리뷰 내용22")
+				.build()
+		));
 	}
 }
---
+++ src/main/java/com/teamddd/duckmap/dto/MyReviewsRes.java
@@ -0,0 +1,19 @@
+package com.teamddd.duckmap.dto;
+
+import java.time.LocalDateTime;
+
+import lombok.Builder;
+import lombok.Getter;
+
+@Getter
+@Builder
+public class MyReviewsRes {
+	private Long id;
+	private Long eventId;
+	private ImageRes eventImage;
+	private String eventStoreName;
+	private LocalDateTime createAt;
+	private int score;
+	private ImageRes reviewImage;
+	private String content;
+}
