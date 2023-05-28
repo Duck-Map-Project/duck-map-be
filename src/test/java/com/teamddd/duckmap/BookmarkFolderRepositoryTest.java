10ed9610ae828ef12a87a15cd781136008b74ad6
yk park <busymidnight17@gmail.com>
2023-05-28 21:28:56 +0900

    Fix: #89 - BookmarkFolderRepository 테스트 코드 수정

--- src/test/java/com/teamddd/duckmap/BookmarkFolderRepositoryTest.java
+++ src/test/java/com/teamddd/duckmap/BookmarkFolderRepositoryTest.java
@@ -16,8 +16,6 @@ import org.springframework.data.domain.PageRequest;
 import org.springframework.transaction.annotation.Transactional;
 
 import javax.persistence.EntityManager;
-import java.time.LocalDate;
-import java.util.ArrayList;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
@@ -37,54 +35,64 @@ public class BookmarkFolderRepositoryTest {
 		User user = User.builder().build();
 		em.persist(user);
 
-		Event event = createEvent(user, "event1", LocalDate.now(), LocalDate.now(), "#hashtag");
-		Event event2 = createEvent(user, "event2", LocalDate.now(), LocalDate.now(), "#hashtag");
-		Event event3 = createEvent(user, "event3", LocalDate.now(), LocalDate.now(), "#hashtag");
+		Event event = createEvent(user, "event1");
+		Event event2 = createEvent(user, "event2");
+		Event event3 = createEvent(user, "event3");
+		Event event4 = createEvent(user, "event4");
 		em.persist(event);
 		em.persist(event2);
 		em.persist(event3);
+		em.persist(event4);
 
-		EventBookmarkFolder eventBookmarkFolder = createEventBookmarkFolder();
+		EventBookmarkFolder eventBookmarkFolder = createEventBookmarkFolder("folder1");
+		EventBookmarkFolder eventBookmarkFolder2 = createEventBookmarkFolder("folder2");
 		em.persist(eventBookmarkFolder);
+		em.persist(eventBookmarkFolder2);
 
 		EventBookmark eventBookmark = createEventBookmark(user, event, eventBookmarkFolder);
 		EventBookmark eventBookmark2 = createEventBookmark(user, event2, eventBookmarkFolder);
-		EventBookmark eventBookmark3 = createEventBookmark(user, event3, eventBookmarkFolder);
-
-		eventBookmark.getEventBookmarkFolder().getEventBookmarks().add(eventBookmark);
-		eventBookmark2.getEventBookmarkFolder().getEventBookmarks().add(eventBookmark2);
-		eventBookmark3.getEventBookmarkFolder().getEventBookmarks().add(eventBookmark3);
+		EventBookmark eventBookmark3 = createEventBookmark(user, event, eventBookmarkFolder2);
+		EventBookmark eventBookmark4 = createEventBookmark(user, event3, eventBookmarkFolder2);
+		EventBookmark eventBookmark5 = createEventBookmark(user, event4, eventBookmarkFolder2);
 
 		em.persist(eventBookmark);
 		em.persist(eventBookmark2);
 		em.persist(eventBookmark3);
+		em.persist(eventBookmark4);
+		em.persist(eventBookmark5);
 
-		PageRequest pageRequest = PageRequest.of(0, 3);
-
+		PageRequest pageRequest = PageRequest.of(0, 2);
+		PageRequest pageRequest2 = PageRequest.of(0, 2);
 		//when
 		Page<BookmarkFolderEventDto> bookmarkedEvents = bookmarkFolderRepository
 				.findBookmarkedEvents(eventBookmarkFolder.getId(), pageRequest);
-
+		Page<BookmarkFolderEventDto> bookmarkedEvents2 = bookmarkFolderRepository
+				.findBookmarkedEvents(eventBookmarkFolder2.getId(), pageRequest2);
 		//then
-		assertThat(bookmarkedEvents).hasSize(3)
+		assertThat(bookmarkedEvents).hasSize(2)
 				.extracting("event.id", "event.storeName", "eventBookmark.id")
 				.containsExactly(Tuple.tuple(event.getId(), "event1", eventBookmark.getId()),
-						Tuple.tuple(event2.getId(), "event2", eventBookmark2.getId()),
-						Tuple.tuple(event3.getId(), "event3", eventBookmark3.getId()));
-
-		assertThat(bookmarkedEvents.getTotalElements()).isEqualTo(3);
+						Tuple.tuple(event2.getId(), "event2", eventBookmark2.getId()));
+		assertThat(bookmarkedEvents.getTotalElements()).isEqualTo(2);
 		assertThat(bookmarkedEvents.getTotalPages()).isEqualTo(1);
+
+		assertThat(bookmarkedEvents2).hasSize(2)
+				.extracting("event.id", "event.storeName", "eventBookmark.id")
+				.containsExactlyInAnyOrder(Tuple.tuple(event.getId(), "event1", eventBookmark3.getId()),
+						Tuple.tuple(event3.getId(), "event3", eventBookmark4.getId()));
+		assertThat(bookmarkedEvents2.getTotalElements()).isEqualTo(3);
+		assertThat(bookmarkedEvents2.getTotalPages()).isEqualTo(2);
 	}
 
-	private Event createEvent(User user, String storeName, LocalDate fromDate, LocalDate toDate, String hashtag) {
-		return Event.builder().user(user).storeName(storeName).fromDate(fromDate).toDate(toDate).hashtag(hashtag).build();
+	private Event createEvent(User user, String storeName) {
+		return Event.builder().user(user).storeName(storeName).build();
 	}
 
 	private EventBookmark createEventBookmark(User user, Event event, EventBookmarkFolder eventBookmarkFolder) {
 		return EventBookmark.builder().user(user).event(event).eventBookmarkFolder(eventBookmarkFolder).build();
 	}
 
-	private EventBookmarkFolder createEventBookmarkFolder() {
-		return EventBookmarkFolder.builder().eventBookmarks(new ArrayList<>()).build();
+	private EventBookmarkFolder createEventBookmarkFolder(String name) {
+		return EventBookmarkFolder.builder().name(name).build();
 	}
 }
