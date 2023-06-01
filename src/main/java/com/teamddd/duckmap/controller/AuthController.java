34c3519c033fdf59cde85658ed39f16888fc4ce1
yk park <busymidnight17@gmail.com>
2023-06-01 10:19:11 +0900

    Fix: RequestBody import 수정

--- src/main/java/com/teamddd/duckmap/controller/AuthController.java
+++ src/main/java/com/teamddd/duckmap/controller/AuthController.java
@@ -5,6 +5,7 @@ import javax.servlet.http.HttpSession;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -13,7 +14,6 @@ import com.teamddd.duckmap.dto.user.auth.LoginUserReq;
 import com.teamddd.duckmap.dto.user.auth.LoginUserRes;
 
 import io.swagger.v3.oas.annotations.Operation;
-import io.swagger.v3.oas.annotations.parameters.RequestBody;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 
