332ddd122f9c9cd18760c064750b673a70a9d614
yk park <busymidnight17@gmail.com>
2023-06-02 00:22:21 +0900

    Refactor: OncePerRequestFilter 상속으로 변경

--- src/main/java/com/teamddd/duckmap/config/security/JwtAuthenticationFilter.java
+++ src/main/java/com/teamddd/duckmap/config/security/JwtAuthenticationFilter.java
@@ -4,33 +4,32 @@ import java.io.IOException;
 
 import javax.servlet.FilterChain;
 import javax.servlet.ServletException;
-import javax.servlet.ServletRequest;
-import javax.servlet.ServletResponse;
 import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
-import org.springframework.web.filter.GenericFilterBean;
+import org.springframework.web.filter.OncePerRequestFilter;
 
 import lombok.RequiredArgsConstructor;
 
 @RequiredArgsConstructor
-public class JwtAuthenticationFilter extends GenericFilterBean {
-	private final JwtTokenProvider jwtTokenProvider;
+public class JwtAuthenticationFilter extends OncePerRequestFilter {
+	private final JwtProvider jwtProvider;
 
 	@Override
-	public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws
-		IOException,
-		ServletException {
+	protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response,
+		FilterChain filterChain) throws ServletException, IOException {
 		// 헤더에서 JWT를 받아옴
-		String token = jwtTokenProvider.resolveToken((HttpServletRequest)request);
+		String token = jwtProvider.resolveToken(request);
 		// 유효한 토큰인지 확인
-		if (token != null && jwtTokenProvider.validateToken(token)) {
+		if (token != null && jwtProvider.validateToken(token)) {
 			// 토큰이 유효하면 토큰으로부터 유저 정보를 받아옴
-			Authentication authentication = jwtTokenProvider.getAuthentication(token);
+			Authentication authentication = jwtProvider.getAuthentication(token);
 			// SecurityContext에 Authentication 객체를 저장
 			SecurityContextHolder.getContext().setAuthentication(authentication);
 		}
-		chain.doFilter(request, response);
+		filterChain.doFilter(request, response);
 	}
+
 }
