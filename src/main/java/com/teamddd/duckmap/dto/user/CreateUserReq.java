de0ac991f28a1965833c61a8581f17cfbde47c5f
yk park <busymidnight17@gmail.com>
2023-06-01 11:08:55 +0900

    Refactor: #142 - 회원가입시 userType 요청하지 않도록 수정

--- src/main/java/com/teamddd/duckmap/dto/user/CreateUserReq.java
+++ src/main/java/com/teamddd/duckmap/dto/user/CreateUserReq.java
@@ -4,8 +4,6 @@ import javax.validation.constraints.Email;
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.Pattern;
 
-import com.teamddd.duckmap.entity.UserType;
-
 import lombok.Getter;
 
 @Getter
@@ -20,6 +18,4 @@ public class CreateUserReq {
 	@Pattern(regexp = "(?=.*[0-9])(?=.*[a-zA-Z])(?=.*\\W)(?=\\S+$).{8,16}",
 		message = "비밀번호는 8~16자 영문 대 소문자, 숫자, 특수문자를 사용하세요.")
 	private String password;
-	@NotBlank
-	private UserType userType; //USER, ADMIN
 }
