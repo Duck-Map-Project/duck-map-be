190fa44e7e4fc87f10b558f6b470728423345d92
yk park <busymidnight17@gmail.com>
2023-06-01 11:06:07 +0900

    Feat: #142 - 회원가입 구현

--- src/main/java/com/teamddd/duckmap/controller/UserController.java
+++ src/main/java/com/teamddd/duckmap/controller/UserController.java
@@ -4,6 +4,7 @@ import java.time.LocalDateTime;
 
 import javax.servlet.http.HttpSession;
 
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -14,6 +15,7 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.teamddd.duckmap.config.security.JwtTokenProvider;
 import com.teamddd.duckmap.dto.ImageRes;
 import com.teamddd.duckmap.dto.Result;
 import com.teamddd.duckmap.dto.user.CreateUserReq;
@@ -21,7 +23,9 @@ import com.teamddd.duckmap.dto.user.CreateUserRes;
 import com.teamddd.duckmap.dto.user.UpdatePasswordReq;
 import com.teamddd.duckmap.dto.user.UpdateUserReq;
 import com.teamddd.duckmap.dto.user.UserRes;
+import com.teamddd.duckmap.entity.User;
 import com.teamddd.duckmap.entity.UserType;
+import com.teamddd.duckmap.repository.UserRepository;
 
 import io.swagger.v3.oas.annotations.Operation;
 import lombok.RequiredArgsConstructor;
@@ -32,14 +36,23 @@ import lombok.extern.slf4j.Slf4j;
 @RequiredArgsConstructor
 @RequestMapping("/users")
 public class UserController {
+	private final PasswordEncoder passwordEncoder;
+	private final JwtTokenProvider jwtTokenProvider;
+	private final UserRepository userRepository;
 
 	@Operation(summary = "회원 가입")
 	@PostMapping
 	public Result<CreateUserRes> createUser(@Validated @RequestBody CreateUserReq createUserReq) {
+		Long id = userRepository.save(User.builder()
+			.email(createUserReq.getEmail())
+			.userType(UserType.USER)
+			.username(createUserReq.getUsername())
+			.password(passwordEncoder.encode(createUserReq.getPassword()))
+			.build()).getId();
 		return Result.<CreateUserRes>builder()
 			.data(
 				CreateUserRes.builder()
-					.id(1L)
+					.id(id)
 					.build()
 			)
 			.build();
