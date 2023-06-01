eae1ec6af038af5a8026f06f905e8ed303a3ba06
yk park <busymidnight17@gmail.com>
2023-06-01 09:45:41 +0900

    Refactor: #124 - findByEmail, findByUsername 반환타입 Optional<User>로 변경

--- src/main/java/com/teamddd/duckmap/repository/UserRepository.java
+++ src/main/java/com/teamddd/duckmap/repository/UserRepository.java
@@ -1,11 +1,13 @@
 package com.teamddd.duckmap.repository;
 
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.teamddd.duckmap.entity.User;
 
 public interface UserRepository extends JpaRepository<User, Long> {
-	User findByEmail(String email);
+	Optional<User> findByEmail(String email);
 
-	User findByUsername(String username);
+	Optional<User> findByUsername(String username);
 }
