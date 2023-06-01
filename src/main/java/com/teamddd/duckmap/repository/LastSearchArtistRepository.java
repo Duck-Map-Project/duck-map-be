c4d2e48d63f16c2beff57601c4a6a97a2e2b8570
yk park <busymidnight17@gmail.com>
2023-06-01 12:19:46 +0900

    Refactor: #143 - findByUserId 구현

--- src/main/java/com/teamddd/duckmap/repository/LastSearchArtistRepository.java
+++ src/main/java/com/teamddd/duckmap/repository/LastSearchArtistRepository.java
@@ -5,4 +5,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import com.teamddd.duckmap.entity.LastSearchArtist;
 
 public interface LastSearchArtistRepository extends JpaRepository<LastSearchArtist, Long> {
+	LastSearchArtist findByUserId(Long userId);
 }
