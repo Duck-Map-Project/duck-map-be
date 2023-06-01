43b9509b88e0241b571668a1b7f32e3e935db603
yk park <busymidnight17@gmail.com>
2023-06-02 00:23:08 +0900

    Refactor: Optional 반환으로 변경

--- src/main/java/com/teamddd/duckmap/repository/LastSearchArtistRepository.java
+++ src/main/java/com/teamddd/duckmap/repository/LastSearchArtistRepository.java
@@ -1,9 +1,11 @@
 package com.teamddd.duckmap.repository;
 
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.teamddd.duckmap.entity.LastSearchArtist;
 
 public interface LastSearchArtistRepository extends JpaRepository<LastSearchArtist, Long> {
-	LastSearchArtist findByUserId(Long userId);
+	Optional<LastSearchArtist> findByUserId(Long userId);
 }
