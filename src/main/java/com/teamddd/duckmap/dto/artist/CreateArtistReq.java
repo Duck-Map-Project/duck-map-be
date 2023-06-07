ede75080a6d05d04338470b26a6f57fe2b7652bb
yurim <yrim113@gmail.com>
2023-06-07 14:35:41 +0900

    Feat: #212 - CreateArtistReq validation test 작성

--- src/main/java/com/teamddd/duckmap/dto/artist/CreateArtistReq.java
+++ src/main/java/com/teamddd/duckmap/dto/artist/CreateArtistReq.java
@@ -2,16 +2,19 @@ package com.teamddd.duckmap.dto.artist;
 
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Positive;
 
 import lombok.Getter;
 
 @Getter
 public class CreateArtistReq {
-	private Long groupId;
-	@NotBlank
-	private String name;
-	@NotNull
-	private String image;
-	@NotNull
+	@Positive
+	@NotNull(message = "아티스트 구분은 필수값입니다")
 	private Long artistTypeId;
+	@Positive
+	private Long groupId;
+	@NotBlank(message = "아티스트 이름은 필수값입니다")
+	private String name;
+	@NotBlank(message = "아티스트 사진은 필수값입니다")
+	private String image;
 }
---
+++ src/test/java/com/teamddd/duckmap/dto/artist/CreateArtistReqTest.java
@@ -0,0 +1,112 @@
+package com.teamddd.duckmap.dto.artist;
+
+import static org.assertj.core.api.Assertions.*;
+
+import java.util.Set;
+
+import javax.validation.ConstraintViolation;
+import javax.validation.Validation;
+import javax.validation.Validator;
+import javax.validation.ValidatorFactory;
+
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.test.util.ReflectionTestUtils;
+
+class CreateArtistReqTest {
+
+	static ValidatorFactory validatorFactory;
+	static Validator validator;
+
+	@BeforeAll
+	static void initialize() {
+		validatorFactory = Validation.buildDefaultValidatorFactory();
+		validator = validatorFactory.getValidator();
+	}
+
+	@AfterAll
+	static void destroy() {
+		validatorFactory.close();
+	}
+
+	@DisplayName("아티스트 구분은 필수값이다")
+	@Test
+	void notNullArtistTypeId() throws Exception {
+		//given
+		CreateArtistReq req = new CreateArtistReq();
+
+		//when
+		Set<ConstraintViolation<CreateArtistReq>> validate = validator.validateProperty(req, "artistTypeId");
+
+		//then
+		assertThat(validate).hasSize(1)
+			.extracting(ConstraintViolation::getMessage)
+			.containsOnly("아티스트 구분은 필수값입니다");
+	}
+
+	@DisplayName("아티스트 구분값은 양수이다")
+	@Test
+	void positiveArtistTypeId() throws Exception {
+		//given
+		CreateArtistReq req = new CreateArtistReq();
+		ReflectionTestUtils.setField(req, "artistTypeId", 0L);
+
+		//when
+		Set<ConstraintViolation<CreateArtistReq>> validate = validator.validateProperty(req, "artistTypeId");
+
+		//then
+		assertThat(validate).hasSize(1)
+			.extracting(ConstraintViolation::getMessage)
+			.containsOnly("0보다 커야 합니다");
+	}
+
+	@DisplayName("그룹값은 양수이다")
+	@Test
+	void positiveGroupId() throws Exception {
+		//given
+		CreateArtistReq req = new CreateArtistReq();
+		ReflectionTestUtils.setField(req, "groupId", 0L);
+
+		//when
+		Set<ConstraintViolation<CreateArtistReq>> validate = validator.validateProperty(req, "groupId");
+
+		//then
+		assertThat(validate).hasSize(1)
+			.extracting(ConstraintViolation::getMessage)
+			.containsOnly("0보다 커야 합니다");
+	}
+
+	@DisplayName("name은 필수값이다")
+	@Test
+	void notBlankName() throws Exception {
+		//given
+		CreateArtistReq req = new CreateArtistReq();
+		ReflectionTestUtils.setField(req, "name", " ");
+
+		//when
+		Set<ConstraintViolation<CreateArtistReq>> validate = validator.validateProperty(req, "name");
+
+		//then
+		assertThat(validate).hasSize(1)
+			.extracting(ConstraintViolation::getMessage)
+			.containsOnly("아티스트 이름은 필수값입니다");
+	}
+
+	@DisplayName("image는 필수값이다")
+	@Test
+	void notBlankImage() throws Exception {
+		//given
+		CreateArtistReq req = new CreateArtistReq();
+		ReflectionTestUtils.setField(req, "image", " ");
+
+		//when
+		Set<ConstraintViolation<CreateArtistReq>> validate = validator.validateProperty(req, "image");
+
+		//then
+		assertThat(validate).hasSize(1)
+			.extracting(ConstraintViolation::getMessage)
+			.containsOnly("아티스트 사진은 필수값입니다");
+	}
+}
