0f4a2a9456631d0b1b36884b5ce7a5aeccec0dbb
yk park <busymidnight17@gmail.com>
2023-06-16 04:15:51 +0900

    Feat: #261 - 비밀번호 재설정 API 구현

---
+++ src/main/java/com/teamddd/duckmap/dto/user/ResetPasswordReq.java
@@ -0,0 +1,16 @@
+package com.teamddd.duckmap.dto.user;
+
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Pattern;
+
+import lombok.Getter;
+
+@Getter
+public class ResetPasswordReq {
+	@NotBlank
+	private String uuid;
+	@NotBlank
+	@Pattern(regexp = "(?=.*[a-zA-Z])(?=.*[0-9])(?=.*[!@#$%^&*]).{8,16}",
+		message = "비밀번호는 8~16자 영문 대 소문자, 숫자, 특수문자를 사용하세요.")
+	private String newPassword;
+}
