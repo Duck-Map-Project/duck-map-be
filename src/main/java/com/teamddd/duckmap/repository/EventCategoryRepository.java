0e06de3fbe4c56b3bffdf483d42de184a38f7529
yurim <yrim113@gmail.com>
2023-06-17 00:20:27 +0900

    Feat: #264 - EventCategory pk 목록으로 EventCategory 목록 조회 기능 구현

--- src/main/java/com/teamddd/duckmap/repository/EventCategoryRepository.java
+++ src/main/java/com/teamddd/duckmap/repository/EventCategoryRepository.java
@@ -1,8 +1,12 @@
 package com.teamddd.duckmap.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.teamddd.duckmap.entity.EventCategory;
 
 public interface EventCategoryRepository extends JpaRepository<EventCategory, Long> {
+
+	List<EventCategory> findByIdIn(List<Long> ids);
 }
