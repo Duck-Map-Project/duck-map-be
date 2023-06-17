94748127b97d427f89ba6eeefac2ea710aa050bd
yurim <yrim113@gmail.com>
2023-06-17 19:37:11 +0900

    Feat: #281 - 인증한 Optional<Member> 조회 기능 구현

--- src/main/java/com/teamddd/duckmap/util/MemberUtils.java
+++ src/main/java/com/teamddd/duckmap/util/MemberUtils.java
@@ -1,9 +1,12 @@
 package com.teamddd.duckmap.util;
 
+import java.util.Optional;
+
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 
 import com.teamddd.duckmap.config.security.UserDetailsImpl;
+import com.teamddd.duckmap.entity.Member;
 import com.teamddd.duckmap.exception.InvalidTokenException;
 
 import lombok.AccessLevel;
@@ -21,5 +24,14 @@ public class MemberUtils {
 			throw new InvalidTokenException();
 		}
 		return ((UserDetailsImpl)principal);
+	}
+
+	public static Optional<Member> getMember() {
+		Authentication auth = SecurityContextHolder.getContext().getAuthentication();
+		Object principal = auth.getPrincipal();
+		if (principal instanceof UserDetailsImpl) {
+			return Optional.of(((UserDetailsImpl)principal).getUser());
+		}
+		return Optional.empty();
 	}
 }
