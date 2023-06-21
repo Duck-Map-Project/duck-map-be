0923c73ca5091d8d82db0d29bde3f276c25c0e74
yk park <busymidnight17@gmail.com>
2023-06-21 19:52:18 +0900

    Feat: #299 - Review pk로 조회 구현

--- src/main/java/com/teamddd/duckmap/service/ReviewService.java
+++ src/main/java/com/teamddd/duckmap/service/ReviewService.java
@@ -6,10 +6,12 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import com.teamddd.duckmap.dto.review.CreateReviewReq;
+import com.teamddd.duckmap.dto.review.ReviewRes;
 import com.teamddd.duckmap.entity.Event;
 import com.teamddd.duckmap.entity.Member;
 import com.teamddd.duckmap.entity.Review;
 import com.teamddd.duckmap.entity.ReviewImage;
+import com.teamddd.duckmap.exception.NonExistentReviewException;
 import com.teamddd.duckmap.repository.ReviewRepository;
 
 import lombok.RequiredArgsConstructor;
@@ -46,4 +48,16 @@ public class ReviewService {
 
 		return review.getId();
 	}
+
+	public Review getReview(Long reviewId) throws NonExistentReviewException {
+		return reviewRepository.findById(reviewId)
+			.orElseThrow(NonExistentReviewException::new);
+	}
+
+	public ReviewRes getReviewById(Long reviewId) throws NonExistentReviewException {
+		return reviewRepository.findById(reviewId)
+			.map(ReviewRes::of)
+			.orElseThrow(NonExistentReviewException::new);
+	}
+
 }
