bfc257ece2726e83b845ff32f9cb24a04467c5e8
yk park <busymidnight17@gmail.com>
2023-06-23 17:45:43 +0900

    Feat: #295 - getMemberById 구현

--- src/main/java/com/teamddd/duckmap/repository/EventLikeRepository.java
+++ src/main/java/com/teamddd/duckmap/repository/EventLikeRepository.java
@@ -1,10 +1,14 @@
 package com.teamddd.duckmap.repository;
 
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.teamddd.duckmap.entity.EventLike;
+import com.teamddd.duckmap.entity.Member;
 
 public interface EventLikeRepository extends JpaRepository<EventLike, Long> {
 	Long countByEventId(Long eventId);
 
+	Optional<Member> getMemberById(Long likeId);
 }
