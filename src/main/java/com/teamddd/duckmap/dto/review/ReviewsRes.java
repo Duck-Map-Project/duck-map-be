38ac428a763e782916d5db01e9a167a743dd3240
yk park <busymidnight17@gmail.com>
2023-06-26 05:11:24 +0900

    Feat: #351 - ReviewsRes 반환하는 of() 구현

--- src/main/java/com/teamddd/duckmap/dto/review/ReviewsRes.java
+++ src/main/java/com/teamddd/duckmap/dto/review/ReviewsRes.java
@@ -1,6 +1,10 @@
 package com.teamddd.duckmap.dto.review;
 
+import java.time.LocalDate;
+
 import com.teamddd.duckmap.dto.ImageRes;
+import com.teamddd.duckmap.entity.Review;
+import com.teamddd.duckmap.entity.ReviewImage;
 
 import lombok.Builder;
 import lombok.Getter;
@@ -11,4 +15,22 @@ public class ReviewsRes {
 	private Long id;
 	private boolean inProgress;
 	private ImageRes image;
+
+	public static ReviewsRes of(Review review) {
+		LocalDate now = LocalDate.now();
+		return ReviewsRes.builder()
+			.id(review.getId())
+			.image(
+				ImageRes.builder()
+					.filename(
+						review.getReviewImages().stream()
+							.map(ReviewImage::getImage)
+							.findFirst()
+							.orElse(null)
+					).build()
+			)
+			.inProgress(review.getEvent().isInProgress(now))
+			.build();
+	}
+
 }
