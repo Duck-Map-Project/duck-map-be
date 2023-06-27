63fc64cff3cd4fdc3f4a35ec651624a47a3b5068
yk park <busymidnight17@gmail.com>
2023-06-27 14:20:58 +0900

    Refactor: #351 - findByEventIdAndMemberId 구현

--- src/main/java/com/teamddd/duckmap/repository/BookmarkRepository.java
+++ src/main/java/com/teamddd/duckmap/repository/BookmarkRepository.java
@@ -1,8 +1,13 @@
 package com.teamddd.duckmap.repository;
 
+import java.util.Optional;
+
+import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.teamddd.duckmap.entity.EventBookmark;
 
-public interface BookmarkRepository extends JpaRepository<EventBookmark, Long>, BookmarkRepositoryCustom {
+public interface BookmarkRepository extends JpaRepository<EventBookmark, Long> {
+	@EntityGraph(attributePaths = {"event"})
+	Optional<EventBookmark> findByEventIdAndMemberId(Long eventId, Long memberId);
 }
