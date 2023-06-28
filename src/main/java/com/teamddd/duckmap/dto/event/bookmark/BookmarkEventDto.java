adae174a5fc52823729f302fba7b6ce9501c27aa
yk park <busymidnight17@gmail.com>
2023-06-28 17:39:14 +0900

    Refactor: #363 - BookmarkEventDto에서 필요한 컬럼만 조회하도록 변경

--- src/main/java/com/teamddd/duckmap/dto/event/bookmark/BookmarkEventDto.java
+++ src/main/java/com/teamddd/duckmap/dto/event/bookmark/BookmarkEventDto.java
@@ -1,19 +1,25 @@
 package com.teamddd.duckmap.dto.event.bookmark;
 
+import java.util.List;
+
 import com.querydsl.core.annotations.QueryProjection;
-import com.teamddd.duckmap.entity.Event;
-import com.teamddd.duckmap.entity.EventBookmark;
+import com.teamddd.duckmap.entity.EventImage;
 
 import lombok.Getter;
 
 @Getter
 public class BookmarkEventDto {
-	private final Event event;
-	private final EventBookmark eventBookmark;
+	private final Long eventId;
+	private final String eventStoreName;
+	private final Long eventBookmarkId;
+	private final List<EventImage> eventImageList;
 
 	@QueryProjection
-	public BookmarkEventDto(Event event, EventBookmark eventBookmark) {
-		this.event = event;
-		this.eventBookmark = eventBookmark;
+	public BookmarkEventDto(Long eventId, String eventStoreName, List<EventImage> eventImageList,
+		Long eventBookmarkId) {
+		this.eventId = eventId;
+		this.eventStoreName = eventStoreName;
+		this.eventImageList = eventImageList;
+		this.eventBookmarkId = eventBookmarkId;
 	}
 }
