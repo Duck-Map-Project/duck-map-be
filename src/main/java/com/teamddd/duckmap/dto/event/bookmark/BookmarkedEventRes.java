24811ebecd3b0f9c7a0ef0652977cd9223bb43de
yk park <busymidnight17@gmail.com>
2023-06-28 17:38:48 +0900

    Feat: #363 - BookmarkedEventRes에 eventId 추가, of() 구현

--- src/main/java/com/teamddd/duckmap/dto/event/bookmark/BookmarkedEventRes.java
+++ src/main/java/com/teamddd/duckmap/dto/event/bookmark/BookmarkedEventRes.java
@@ -1,6 +1,7 @@
 package com.teamddd.duckmap.dto.event.bookmark;
 
 import com.teamddd.duckmap.dto.ImageRes;
+import com.teamddd.duckmap.entity.EventImage;
 
 import lombok.Builder;
 import lombok.Getter;
@@ -9,6 +10,26 @@ import lombok.Getter;
 @Builder
 public class BookmarkedEventRes {
 	private Long id;
+	private Long eventId;
 	private String storeName;
 	private ImageRes image;
+
+	public static BookmarkedEventRes of(BookmarkEventDto bookmarkEventDto) {
+		return BookmarkedEventRes.builder()
+			.id(bookmarkEventDto.getEventBookmarkId())
+			.eventId(bookmarkEventDto.getEventId())
+			.storeName(bookmarkEventDto.getEventStoreName())
+			.image(
+				ImageRes.builder()
+					.filename(
+						bookmarkEventDto.getEventImageList().stream()
+							.filter(EventImage::isThumbnail)
+							.map(EventImage::getImage)
+							.findFirst()
+							.orElse(null)
+					)
+					.build()
+			)
+			.build();
+	}
 }
