1b17d522bae91ff60760d610d601cf7bb58a4d6f
yk park <busymidnight17@gmail.com>
2023-07-06 01:28:55 +0900

    Feat: #392 - BaseByEntity JPA Auditing 적용

--- src/main/java/com/teamddd/duckmap/DuckmapApplication.java
+++ src/main/java/com/teamddd/duckmap/DuckmapApplication.java
@@ -3,10 +3,8 @@ package com.teamddd.duckmap;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
-import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @ConfigurationPropertiesScan
-@EnableJpaAuditing
 @SpringBootApplication
 public class DuckmapApplication {
 
