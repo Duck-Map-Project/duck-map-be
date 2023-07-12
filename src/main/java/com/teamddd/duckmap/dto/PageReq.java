e359aa8232ed3b91f0164e7401a2c3fce3f56175
yk park <busymidnight17@gmail.com>
2023-07-12 17:01:55 +0900

    Refactor: #427 - PageReq defaultValue 설정

--- src/main/java/com/teamddd/duckmap/dto/PageReq.java
+++ src/main/java/com/teamddd/duckmap/dto/PageReq.java
@@ -1,11 +1,14 @@
 package com.teamddd.duckmap.dto;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Builder;
 import lombok.Getter;
 
 @Builder
 @Getter
 public class PageReq {
+	@Schema(defaultValue = "0")
 	private int pageNumber;
+	@Schema(defaultValue = "20")
 	private int pageSize;
 }
