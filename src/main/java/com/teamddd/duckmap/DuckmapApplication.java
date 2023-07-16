2b216ab2a751b863f9770d204d7256ab198c3bd7
yk park <busymidnight17@gmail.com>
2023-07-16 18:52:36 +0900

    Feat: CORS 설정을 위한 Bean 등록

--- src/main/java/com/teamddd/duckmap/DuckmapApplication.java
+++ src/main/java/com/teamddd/duckmap/DuckmapApplication.java
@@ -3,6 +3,9 @@ package com.teamddd.duckmap;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @ConfigurationPropertiesScan
 @SpringBootApplication
@@ -11,6 +14,19 @@ public class DuckmapApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(DuckmapApplication.class, args
 		);
+	}
+
+	@Bean
+	public WebMvcConfigurer corsConfigurer() {
+		return new WebMvcConfigurer() {
+			@Override
+			public void addCorsMappings(CorsRegistry registry) {
+				registry.addMapping("/**")
+					.allowedOrigins(
+						"http://duck-map.s3-website.ap-northeast-2.amazonaws.com/",
+						"http://localhost:3000");
+			}
+		};
 	}
 
 }
