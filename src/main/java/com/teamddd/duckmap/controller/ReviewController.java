3dadd6bc4da2c0795ac2e5b7b81bb8e8631dd21b
yk park <busymidnight17@gmail.com>
2023-08-04 15:11:49 +0900

    Refactor: #523 - getReviewsResList에서 검색된 아티스트를 해당 member의 마지막 검색 아티스트로 저장한다

--- src/main/java/com/teamddd/duckmap/controller/ReviewController.java
+++ src/main/java/com/teamddd/duckmap/controller/ReviewController.java
@@ -75,6 +75,7 @@ public class ReviewController {
 	@Operation(summary = "리뷰 목록 조회", description = "artist, 날짜 기준 리뷰 목록 조회 기능 구현")
 	@GetMapping
 	public Page<ReviewsRes> getReviews(PageReq pageReq, @ModelAttribute ReviewSearchParam reviewSearchParam) {
+
 		LocalDate today = LocalDate.now();
 
 		Pageable pageable = PageRequest.of(pageReq.getPageNumber(), pageReq.getPageSize());
