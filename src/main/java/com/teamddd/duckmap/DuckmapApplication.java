e7c7e0ea032d678b799e9f41255c8fb122ddb018
yk park <busymidnight17@gmail.com>
2024-05-02 23:48:10 +0900

    add: OpenAPIDefinition annotation

--- src/main/java/com/teamddd/duckmap/DuckmapApplication.java
+++ src/main/java/com/teamddd/duckmap/DuckmapApplication.java
@@ -4,7 +4,11 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.servers.Server;
+
 @ConfigurationPropertiesScan
+@OpenAPIDefinition(servers = {@Server(url = "https://duckmap.shop", description = "duckmap")})
 @SpringBootApplication
 public class DuckmapApplication {
 
